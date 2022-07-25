0a9307c38303d057500313aabf388ef882823f63
Joyce Chen <joyceideas@outlook.com>
2022-07-25 04:23:20 -0400

    updating JavaScript codes

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,40 @@
+// 1. Start Appication
+var generateBtn = document.querySelector("#generate");
+
+// 2. List of different variables
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var symbols = ["!", "@", "#", "~", "$", "%", "^", "&", "*", "_", "-", "+", "=", "[", "]", "{", "}", "|", "\", ", ", ", "<", ">", "?", "/"];
+
+
+
+function generatePassword() {
+  var length = prompt(
+    "How long do you want your password to be? The numbers must be 8-124.");
+  
+    if (length < 8 || length > 124) {
+    return alert("Your password isn't the right length")}
+  
+    var hasLowerCase = confirm(
+    "Do you want lowercase letters in your password?");
+  
+    var hasUpperCase = confirm(
+    "Do you want uppercase letters in your password?");
+  
+    var hasNumbers = confirm(
+    "Do you want numbers in your password?");
+  
+    var hasSymbols = confirm(
+    "Do you want symbols in your password?");
+}
+
+// Empty Array
+var possibleCharacxters = [];
+var uppercaseLetters=[];
+var genPwd ="";
+var pwdOptions=[];
+
 // Assignment code here
 var generateBtn = document.querySelector("#generate");
 // 1. Start Appication
@@ -12,26 +49,13 @@ var generateBtn = document.querySelector("#generate");
 // 5. Finally, based on the length they selected at the beginning, we run the random number generator that many times to select characters for their password.
 
 // 6. Then we write that generated password to the main box in our html.
-function generatePassword() {
-  var length = prompt(
-    "How long do you want your password to be? It must be 8-124"
-  );
-  if (length < 8 || length > 124) {
-    return alert("Your password isn't the right length")
-  }
-  var hasLowerCase = confirm(
-    "Do you want lower case in your password"
-  );
-  var length = comfirm(
-    "How long do you want your password to be? It must be 8-124"
-  );
-}
-var builtString = ''; //'11111111'
-for (var i = 0; i < 10; i++) {
-  var characters = 1; //generate some random character
-  builtString.length * characters;
-}
 
+
+
+// for (var i = 0; i < 10; i++) {
+//   var characters = 1; //generate some random character
+//   builtString.length * characters;
+// }
 
 
 
