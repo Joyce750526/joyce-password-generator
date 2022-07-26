7852856a70c26f4af58642e34b6c0d35c842d452
Joyce Chen <joyceideas@outlook.com>
2022-07-25 23:46:24 -0400

    fixing broken link in html

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,66 +8,70 @@ var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var symbols = ["!", "@", "#", "~", "$", "%", "^", "&", "*", "_", "-", "+", "=", "[", "]", "{", "}", "|", "\", ", ", ", "<", ">", "?", "/"];
 
 
-
+// Created a series of prompts to store the user's input with assigned variables
 function generatePassword() {
   var length = prompt(
     "How long do you want your password to be? The numbers must be 8-124.");
-  
-    if (length < 8 || length > 124) {
-    return alert("Your password isn't the right length")}
-  
-    var hasLowerCase = confirm(
+
+  if (length < 8 || length > 124) {
+    return alert("Your password isn't the right length")
+  }
+
+  var hasLowerCase = confirm(
     "Do you want lowercase letters in your password?");
-  
-    var hasUpperCase = confirm(
+
+  var hasUpperCase = confirm(
     "Do you want uppercase letters in your password?");
-  
-    var hasNumbers = confirm(
+
+  var hasNumbers = confirm(
     "Do you want numbers in your password?");
-  
-    var hasSymbols = confirm(
+
+  var hasSymbols = confirm(
     "Do you want symbols in your password?");
+
+  for (var i = 0; i < length; i++) {
+    var characters = 1; 
+    genPwd = genPwd + characters;
+  }
+  console.log(genPwd);
+  for (var i = 0; i < length; i++);
+  
+  var randomNum = Math.floor(Math.random() * pwdOptions.length);
+  genPwd = genPwd.concat(pwdOptions[randomNum]);
+
+  return genPwd;
+
+
+  // 3 empty arrays and 1 empty string to store user's inputs for later use.
+  var possibleCharacxters = [];
+  var uppercaseLetters = [];
+  var genPwd = "";
+  var pwdOptions = [];
+
+  // This is to concatenate a desired characters for user's preferred input
+  if (hasLowerCase === true) {
+    pwdOptions = pwdOptions.concat(lowercase);
+  }
+
+  if (hasUpperCase === true) {
+    pwdOptions = pwdOptions.concat(uppercase);
+  }
+
+  if (hasSymbols === true) {
+    pwdOptions = pwdOptions.concat(symbols);
+  }
+  if (hasNumbers === true) {
+    pwdOptions = pwdOptions.concat(numbers);
+  }
+
+  // Write password to the #password input
+  function writePassword() {
+    var generateBtn = document.querySelector("generate"); function writePassword() {
+      var password = generatePassword();
+      var passwordText = document.querySelector("#password");
+      passwordText.value = password;
+    }
+  }
+  // Add event listener to generate button
+  generateBtn.addEventListener("click", writePassword);
 }
-
-// Empty Array
-var possibleCharacxters = [];
-var uppercaseLetters=[];
-var genPwd ="";
-var pwdOptions=[];
-
-// Assignment code here
-var generateBtn = document.querySelector("#generate");
-// 1. Start Appication
-
-// 2. User is presented with prompts to select length of password and
-// what types of characters they want in their password.
-
-// 3. We use that data to create a password for them, using arrays of the the different kinds of characters they selected.
-
-// 4. We use a random number generator to select from that array of all the different characters.
-
-// 5. Finally, based on the length they selected at the beginning, we run the random number generator that many times to select characters for their password.
-
-// 6. Then we write that generated password to the main box in our html.
-
-
-
-// for (var i = 0; i < 10; i++) {
-//   var characters = 1; //generate some random character
-//   builtString.length * characters;
-// }
-
-
-
-// Get references to the #generate element
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
