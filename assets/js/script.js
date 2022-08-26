72d31819fcadd4ab83da70170d819ec43c7b0667
Joyce Chen <joyceideas@outlook.com>
2022-08-26 13:30:08 -0400

    fixing js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,11 @@
 // 1. Start Appication
 var generateBtn = document.querySelector("#generate");
 
+// 3 empty arrays and 1 empty string to store user's inputs for later use.
+var possibleCharacxters = [];
+var uppercaseLetters = [];
+
+
 // 2. List of different variables
 var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
@@ -10,6 +15,9 @@ var symbols = ["!", "@", "#", "~", "$", "%", "^", "&", "*", "_", "-", "+", "=", 
 
 // Created a series of prompts to store the user's input with assigned variables
 function generatePassword() {
+  var genPwd = "";
+  var pwdOptions = [];
+
   var length = prompt(
     "How long do you want your password to be? The numbers must be 8-124.");
 
@@ -17,21 +25,7 @@ function generatePassword() {
     return alert("Your password isn't the right length")
   }
 
-  // This is to concatenate a desired characters for user's preferred input
-if (hasLowerCase === true) {
-  pwdOptions = pwdOptions.concat(lowercase);
-}
-  if (hasUpperCase === true) {
-    pwdOptions = pwdOptions.concat(uppercase);
-  };
 
-  if (hasSymbols === true) {
-    pwdOptions = pwdOptions.concat(symbols);
-  };
-
-    if (hasNumbers === true) {
-      pwdOptions = pwdOptions.concat(numbers);
-    };
   var hasLowerCase = confirm(
     "Do you want lowercase letters in your password?");
 
@@ -44,35 +38,42 @@ if (hasLowerCase === true) {
   var hasSymbols = confirm(
     "Do you want symbols in your password?");
 
+  // This is to concatenate a desired characters for user's preferred input
+  if (hasLowerCase === true) {
+    pwdOptions = pwdOptions.concat(lowercase);
+  }
+  if (hasUpperCase === true) {
+    pwdOptions = pwdOptions.concat(uppercase);
+  };
+
+  if (hasSymbols === true) {
+    pwdOptions = pwdOptions.concat(symbols);
+  };
+
+  if (hasNumbers === true) {
+    pwdOptions = pwdOptions.concat(numbers);
+  };
+  console.log(pwdOptions);
+
   for (var i = 0; i < length; i++) {
-    var characters = 1;
-    genPwd = genPwd + characters;
+    var randomNum = Math.floor(Math.random() * pwdOptions.length);
+    genPwd = genPwd.concat(pwdOptions[randomNum]);
   }
   console.log(genPwd);
-  for (var i = 0; i < length; i++);
-
-  var randomNum = Math.floor(Math.random() * pwdOptions.length);
-  genPwd = genPwd.concat(pwdOptions[randomNum]);
 
   return genPwd;
-  
+
 }
 
-// 3 empty arrays and 1 empty string to store user's inputs for later use.
-var possibleCharacxters = [];
-var uppercaseLetters = [];
-var genPwd = "";
-var pwdOptions = [];
 
 
 
-    // Write password to the #password input
-    function writePassword() {
-      var generateBtn = document.querySelector("generate"); function writePassword() {
-        var password = generatePassword();
-        var passwordText = document.querySelector("#password");
-        passwordText.value = password;
-      }
-      // Add event listener to generate button
-      generateBtn.addEventListener("click", writePassword);
-    };
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+  // Add event listener to generate button
+};
+generateBtn.addEventListener("click", writePassword);
